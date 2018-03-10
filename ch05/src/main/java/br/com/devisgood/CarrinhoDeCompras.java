378aff71dd8f7e7289f42017018014dc172d0037
Filipe Canatto <filipecanatto@gmail.com>
2018-03-10 17:11:15 -0300

    end of chapter five

--- ch05/src/main/java/br/com/devisgood/CarrinhoDeCompras.java
+++ ch05/src/main/java/br/com/devisgood/CarrinhoDeCompras.java
@@ -3,18 +3,33 @@ package br.com.devisgood;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+
 import br.com.devisgood.Item;
 
-public	class	CarrinhoDeCompras	{
-	private	List<Item>	itens;
-	public	CarrinhoDeCompras()	{
-					this.itens	=	new	ArrayList<Item>();
+public class CarrinhoDeCompras {
+	private List<Item> itens;
+
+	public CarrinhoDeCompras() {
+		this.itens = new ArrayList<Item>();
+	}
+
+	public void adiciona(Item item) {
+		this.itens.add(item);
+	}
+
+	public List<Item> getItens() {
+		return Collections.unmodifiableList(itens);
 	}
 	
-	public	void	adiciona(Item	item)	{
-		this.itens.add(item);
-}
-public	List<Item>	getItens()	{
-		return	Collections.unmodifiableList(itens);
-}
+	public double calculaMaiorValor(){
+		if (getItens().size() == 0) return 0;
+		
+		double maior = getItens().get(0).getValorTotal();
+		
+		for (Item item :getItens()){
+			if (maior < item.getValorTotal()) maior = item.getValorTotal();
+		}
+		
+		return maior;
+	}
 }
--- ch05/src/test/java/br/com/devisgood/MaiorPrecoTest.java
+++ ch05/src/test/java/br/com/devisgood/MaiorPrecoTest.java
@@ -10,12 +10,8 @@ public class MaiorPrecoTest {
 		// given
 		CarrinhoDeCompras carrinhoDeCompras = new CarrinhoDeCompras();
 		
-		// when
-		MaiorPreco algoritimo = new MaiorPreco();
-		double valor = algoritimo.encontra(carrinhoDeCompras);
-		
 		//then
-		Assert.assertEquals(0.0, valor, 0.0001);
+		Assert.assertEquals(0.0, carrinhoDeCompras.calculaMaiorValor(), 0.0001);
 		
 	}
 	
@@ -26,12 +22,8 @@ public class MaiorPrecoTest {
 		Item item = new Item("computador", 1, 1000);
 		carrinho.adiciona(item);
 		
-		//when
-		MaiorPreco algoritimo = new MaiorPreco();
-		double valor = algoritimo.encontra(carrinho);
-		
 		// then
-		Assert.assertEquals(1000, valor, 0.0001);
+		Assert.assertEquals(1000, carrinho.calculaMaiorValor(), 0.0001);
 	}
 	
 	@Test
@@ -43,12 +35,8 @@ public class MaiorPrecoTest {
 		carrinho.adiciona(new Item("mesa", 2, 500));
 		carrinho.adiciona(new Item("cadeira", 4, 250));
 		
-		//when
-		MaiorPreco algoritimo = new MaiorPreco();
-		double valor = algoritimo.encontra(carrinho);
-		
 		// then
-		Assert.assertEquals(1250.00, valor, 0.0001);
+		Assert.assertEquals(1000.00, carrinho.calculaMaiorValor(), 0.0001);
 	}
 	
 
